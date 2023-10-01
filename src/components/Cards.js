3490d864279ae168b0a5ac7f23d62b36b48a686b
Balbaa <omareidbalbaa@gmail.com>
2023-10-02 00:05:01 +0300

    Created Card Items

---
+++ src/components/Cards.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import CardItem from './CardItem'
+import "./Cards.css";
+
+
+function Cards() {
+  return (
+    <div className='cards'>
+        <h1>Check out these EPIC Destination</h1>
+        <div className='cards__container'>
+            <div className='cards__wrapper'>
+                <ul className='cards__items'>
+                    <CardItem
+                    src="images/img-9.jpg"
+                    text="Explore the hidden waterfall deep inside the Amazon Jungle"
+                    label="Adventure"
+                    path='/services'
+                    />
+                    <CardItem
+                    src="images/img-1.jpg"
+                    text="Feel the nature and see what's beyond beauty"
+                    label="Nature"
+                    path='/services'
+                    />
+                </ul>
+                <ul className='cards__items'>
+                    
+                    <CardItem
+                    src="images/img-2.jpg"
+                    text="Travel through the Island of Bali in a Private Cruise"
+                    label="Luxary"
+                    path='/services'
+                    />
+                    <CardItem
+                    src="images/img-3.jpg"
+                    text="Explore the hidden waterfall deep inside the Amazon Jungle"
+                    label="Adventure"
+                    path='/services'
+                    />
+                    <CardItem
+                    src="images/img-4.jpg"
+                    text="Feel the nature and see what's beyond beauty"
+                    label="Skyscaping"
+                    path='/services'
+                    />
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Cards
