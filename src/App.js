eca007c245cd71721720127977b687172b725b67
Balbaa <omareidbalbaa@gmail.com>
2023-10-02 02:39:59 +0300

    Created products and services pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from './components/pages/Home'
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" exact Component={Home} />
+          <Route path="/Services" exact Component={Services} />
+          <Route path="/Products" exact Component={Products} />
+          <Route path="/sign-up" exact Component={SignUp} />
         </Routes>
       </Router>
     </>
