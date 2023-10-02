c478510c88d1bdcee0744500a15cd6bfe7bded21
Balbaa <omareidbalbaa@gmail.com>
2023-10-02 19:28:40 +0300

    modify routers

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from './components/pages/Home'
-import Services from './components/pages/Services';
-import Products from './components/pages/Products';
-import SignUp from './components/pages/SignUp';
+import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
   return (
     <>
-      {/* <Router> */}
       <HashRouter basename="/">
         <Navbar />
         <Routes>
-          <Route path="/" exact Component={Home} />
-          <Route path="/Services" exact Component={Services} />
-          <Route path="/Products" exact Component={Products} />
-          <Route path="/sign-up" exact Component={SignUp} />
+          <Route path="/" Component={Home} />
+          <Route path="/Services" Component={Services} />
+          <Route path="/Products" Component={Products} />
+          <Route path="/sign-up" Component={SignUp} />
         </Routes>
-        </HashRouter>
-      {/* </Router> */}
+      </HashRouter>
     </>
   );
 }
